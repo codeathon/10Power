255a4ef8d2c2a20fa54b2dec26b1f0f2f8e092d3
codeathon <rohit.ainapure@gmail.com>
2012-07-28 22:36:17 -0700

    First Commit

---
+++ src/com/android/power/Power10Activity.java
@@ -0,0 +1,16 @@
+package com.android.power;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+
+public class Power10Activity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.screen2);
+    }
+}
